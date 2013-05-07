5f0c6c1bbf9609194d5721810f817139e5329d2d
Alan Medlar <amedlar@gmail.com>
2013-05-07 17:13:54 +0300

    no correct sga script

--- sga_nocorrect.py
+++ sga_nocorrect.py
@@ -42,20 +42,6 @@ def rm(f) :
     except :
         pass
 
-
-def sga_init(fastq, m, f, q) :
-    global threads
-
-    n = clipext(fastq)
-    o = "%s.m%df%dq%d.fq" % (n, m, f, q)
-    u = "%s.m%df%dq%d.uniq.fq" % (n, m, f, q)
-
-    run("sga preprocess -p 0 -m %d -f %d -q %d -o %s %s" % (m, f, q, o, fastq))
-    run_shell("grep -B1 '^\+$' %s | grep -v '^\+$' | grep -v '^--$' | sort | uniq | python ./seq2fa.py > %s" % (o, u))
-    run("sga index -a ropebwt -t %d --no-reverse %s" % (threads, u))
-
-    return u
-
 def cleanup(files) :
     global verbose
 
@@ -68,31 +54,33 @@ def cleanup(files) :
 def clipext(f) :
     return '.'.join(f.split('.')[:-1])
 
-def sga(fasta, k, kmer_threshold, merge_overlap, assemble_overlap) :
+def sga_init(fastq, m, f, q) :
     global threads
 
-    out = "assembly.%s.k%d" % (clipext(fasta), k)
+    n = clipext(fastq)
+    o = "%s.m%df%dq%d.fq" % (n, m, f, q)
+    u = "%s.m%df%dq%d.uniq.fq" % (n, m, f, q)
 
-    run("sga correct -k %d -x %d --discard -t %d -o correct.fa %s" % (k, kmer_threshold, threads, fasta))
-    run("sga index -a ropebwt -t %d correct.fa" % (threads))
-    run("sga filter -x 2 -t %d --homopolymer-check --low-complexity-check correct.fa" % (threads))
+    run("sga preprocess -p 0 -m %d -f %d -q %d -o %s %s" % (m, f, q, o, fastq))
+    run_shell("grep -B1 '^\+$' %s | grep -v '^\+$' | grep -v '^--$' | sort | uniq | python ./seq2fa.py > %s" % (o, u))
+    #run("sga index -a ropebwt -t %d --no-reverse %s" % (threads, u))
 
-    run("sga fm-merge -m %d -t %d -o merged.fa correct.filter.pass.fa" % (merge_overlap, threads))
-    run("sga index -d 1000000 -t %d merged.fa" % (threads))
-    run("sga rmdup -t %d merged.fa" % (threads))
-    run("sga overlap -m %d -t %d merged.rmdup.fa" % (merge_overlap, threads))
-
-    run("sga assemble -m %d -g 0 -r 10 -o %s merged.rmdup.asqg.gz" % (assemble_overlap, out))
-
-    cleanup(glob("correct.*") + glob("merged.*") + glob(clipext(clipext(fasta)) + ".*"))
+    return u
 
 # XXX should be filter out everything that cannot be corrected?
 def sga_correct(fasta, k, kmer_threshold) :
     global threads
 
-    run("sga correct -k %d -x %d --discard -t %d -o correct.fa %s" % (k, kmer_threshold, threads, fasta))
+    #run("sga correct -k %d -x %d --discard -t %d -o correct.fa %s" % (k, kmer_threshold, threads, fasta))
+    #run("sga index -a ropebwt -t %d correct.fa" % (threads))
+    #run("sga filter -x 2 -t %d --homopolymer-check --low-complexity-check correct.fa" % (threads))
+
+    run_shell("cp %s correct.fa" % (fasta))
     run("sga index -a ropebwt -t %d correct.fa" % (threads))
-    run("sga filter -x 2 -t %d --homopolymer-check --low-complexity-check correct.fa" % (threads))
+    if kmer_threshold != 0 :
+        run("sga filter -k %d -x %d -t %d --homopolymer-check --low-complexity-check correct.fa" % (k, kmer_threshold, threads))
+    else :
+        run("sga filter --no-kmer-check -t %d --homopolymer-check --low-complexity-check correct.fa" % (threads))
 
 def sga_build(merge_overlap) :
     global threads
@@ -117,8 +105,8 @@ def main() :
 
     f = sga_init(sys.argv[1], 50, 0, 30)
 
-    k_values =  [27,29,31,33,35,37,39,41]
-    kt_values = [1,2,3,4,5,6,7,8,9]
+    k_values =  [23,25,27,29,31,33,35,37,39,41]
+    kt_values = [0,1,2,3,4,5,6,7,8,9]
     mo_values = [20,25,30,35,40,45]
     ao_values = [20,25,30,35,40,45]
 
